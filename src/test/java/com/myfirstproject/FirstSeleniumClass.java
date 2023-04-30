566eb79ffd5d2318a9f8bfc1eaaa41d1d6b0ea82
nnerminbasaran <n.nerminbasaran@gmail.com>
2023-04-30 21:06:08 +0300

    30.04.2023

---
+++ src/test/java/com/myfirstproject/FirstSeleniumClass.java
@@ -0,0 +1,19 @@
+package com.myfirstproject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstSeleniumClass {
+    public static void main(String[] args) {
+//      this is one way to create an automation script
+//      We will not use main method later on
+
+        System.setProperty("webdriver.chrome.driver","./drivers/chromedriver");
+
+//      create driver
+        WebDriver driver = new ChromeDriver();
+
+//      navigate to techpro
+        driver.get("https://www.techproeducation.com");
+    }
+}
