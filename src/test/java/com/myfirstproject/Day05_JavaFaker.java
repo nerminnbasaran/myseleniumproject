ac42912367e72dd00c49b06f6527916fb2f826c3
nnerminbasaran <n.nerminbasaran@gmail.com>
2023-05-05 17:28:21 +0300

    05.05.2023

---
+++ src/test/java/com/myfirstproject/Day05_JavaFaker.java
@@ -0,0 +1,56 @@
+package com.myfirstproject;
+
+import com.github.javafaker.Faker;
+import org.junit.Test;
+
+public class Day05_JavaFaker {
+/*
+    Data : username,password,phone number,test environment, city,ssn,url,job title...
+    -Where do you get your test data in your company?
+    -BA - Business Analyst > writes the acceptance criteria or requirement
+    -Test Lead
+    -Manual Tester
+    -Tech Lead(Team Lead)
+    -PO - Product Owner > maintains product backlog
+    -Developers
+    -From Database
+    -From API calls
+    in addition to all these, we can use java faker if we need some fake data
+
+*/
+    @Test
+    public void javaFaker(){
+//      1. Create a Faker object
+        Faker faker = new Faker();
+//      2. User faker object or generate fake test data
+        String firstName = faker.name().firstName();
+        System.out.println(firstName);
+
+//      last name
+        System.out.println(faker.name().lastName());
+//      title
+        System.out.println(faker.name().title());
+//      city
+        System.out.println(faker.address().city());
+//      state
+        System.out.println("State: "+faker.address().state());
+//      country
+        System.out.println("Country: "+faker.address().country());
+//      address
+        System.out.println("Full Address "+faker.address().fullAddress());
+//      zip code
+        System.out.println("Zip code: "+faker.address().zipCode());
+//      random number: 10 digit
+        System.out.println(faker.number().digits(10));
+//      cell phone number
+        System.out.println(faker.phoneNumber().cellPhone());
+//      ssn
+        System.out.println(faker.idNumber().ssnValid());
+//      email
+        System.out.println(faker.internet().emailAddress());
+//      birthday
+        System.out.println(faker.date().birthday());
+
+
+    }
+}
---
+++ src/test/java/com/myfirstproject/Day05_RadioButton.java
@@ -0,0 +1,35 @@
+package com.myfirstproject;
+
+import com.myfirstproject.utilities.TestBase;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class Day05_RadioButton extends TestBase {
+
+//      https://testcenter.techproeducation.com/index.php?page=radio-buttons
+//      Click on red
+//      Click on Football
+    @Test
+    public void radioButtonTest(){
+//      go to the page
+        driver.get("https://testcenter.techproeducation.com/index.php?page=radio-buttons");
+//      locating red
+        WebElement red = driver.findElement(By.id("red"));
+//      clicking on red
+        red.click();
+//      verifying if red is checked
+        Assert.assertTrue(red.isSelected());
+//      locating Football
+         WebElement football = driver.findElement(By.id("football"));
+//      clicking on Football
+        football.click();
+//      verifying if football is checked
+        Assert.assertTrue(football.isSelected());
+
+
+
+
+    }
+}
