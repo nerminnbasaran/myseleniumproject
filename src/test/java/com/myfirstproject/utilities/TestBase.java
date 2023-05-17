74075908785de1bc49e11674d3c129bcfe258ea4
nnerminbasaran <n.nerminbasaran@gmail.com>
2023-05-17 14:02:57 +0300

    17.05.2023

--- src/test/java/com/myfirstproject/utilities/TestBase.java
+++ src/test/java/com/myfirstproject/utilities/TestBase.java
@@ -1,9 +1,14 @@
 package com.myfirstproject.utilities;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -15,6 +20,47 @@ import java.util.Date;
 
 public abstract class TestBase {
     protected static WebDriver driver;
+    protected static ExtentReports extentReports;
+    protected static ExtentHtmlReporter extentHtmlReporter;
+    protected static ExtentTest extentTest;
+    @BeforeClass
+    public static void extentReportsSetup(){
+//        PATH
+        String now = new SimpleDateFormat("yyyMMddhhmmss").format(new Date());
+        String path = System.getProperty("user.dir")+"/test-output/Reports"+now+"extent_reports.html";
+
+//        Create the HTML template using extent html reporter
+        extentHtmlReporter = new ExtentHtmlReporter(path);
+
+//        Create extent report
+        extentReports = new ExtentReports();
+
+//        **********ADDING CUSTOM SYSTEM INFORMATION*************
+        extentReports.setSystemInfo("Application Name","Apple M1 Unit");
+        extentReports.setSystemInfo("Test Environment","Regression");
+        extentReports.setSystemInfo("Browser","Chrome");
+        extentReports.setSystemInfo("Team Name","Eagles");
+        extentReports.setSystemInfo("SQA","Nermin");
+        extentReports.setSystemInfo("Feature Number","FE1056");
+
+//        *************DOCUMENT INFORMATION***********************
+        extentHtmlReporter.config().setReportName("My Regression Report");
+        extentHtmlReporter.config().setDocumentTitle("My Regression Extent Reports");
+//        **************DONE WITH CONFIGURATIONS********************
+
+//        connecting extent reports and extent html reporter
+        extentReports.attachReporter(extentHtmlReporter);
+
+//        ***************CREATE EXTENT TEST THAT IS ALSO KNOWN AS LOGGER**********
+        extentTest=extentReports.createTest("MY REGRESSION","MY FIRST EXTENT REPORT");
+
+    }
+    @AfterClass
+    public static void tearDownClass(){
+//        generate the report
+        extentReports.flush();
+    }
+
     @Before
     public void setUp(){
         WebDriverManager.chromedriver().setup();
@@ -22,25 +68,29 @@ public abstract class TestBase {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         driver.manage().window().maximize();
     }
+
     @After
     public void tearDown() throws InterruptedException {
         Thread.sleep(5000);
         driver.quit();
     }
 
+//    This method captures screenshot of the entire page
     public void takeScreenshotOfTheEntirePage() throws IOException {
 
 //        1. TakeScreenShot class with getScreenShotAs method to capture the screenshot
        File image = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 
 //        2. Create a path to save the image
-//           Create a data for giving dynamic name otherwise the screenshots overrides
+//           Create a date for giving dynamic name otherwise the screenshots overrides
         String now = new SimpleDateFormat("yyyMMddhhmmss").format(new Date());//getting local date in this format
+
 //                       CURRENT PROJECT FOLDER        foldername  subfoldername   imagename
         String path = System.getProperty("user.dir")+"/test-output/Screenshots/" +now+"image.png";
 
 //        3. Save the image in the path as a file
         FileUtils.copyFile(image,new File(path));
+
 //        FileUtils.copyFile(FILE,FILE PATH); COPY FILE TO THAT FILE PATH
 
     }
